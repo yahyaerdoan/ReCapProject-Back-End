cf39745cab9528f0cb55df921105603b7ba8376b
yahyaerdogan <yahyaerdogan@YAHYAERDOGAN>
2021-03-25 21:58:33 +0300

    ExceptionMiddleware Operasyonu Eklendi

---
+++ Core/Extensions/ValidationErrorDetails.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation.Results;
+using System.Collections.Generic;
+
+namespace Core.Extensions
+{
+    //Doğrulama hatası için liste oluşturması için ValidationErrorDetails oluşturduk
+    public class ValidationErrorDetails : ErrorDetails
+    {
+        public IEnumerable<ValidationFailure> ValidationErrors { get; set; }
+    }
+}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -107,6 +107,8 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.ConfigureCustomExceptionMiddleware();
+
             app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader()); //AllowAnyHeader : solumdaki adreslerden ne gelirse gelsin güveniyorum. Onlara açýðým diyor.
 
             app.UseHttpsRedirection();
